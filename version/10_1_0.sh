6c43b89e3f95c25a31df452d4eb79ad6ea0dc5c7
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-05-16 18:28:38 +0530

    Create 10_1_0.sh

---
+++ version/10_1_0.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+
+NODE_VERSION=10.1.0
+NPM_VERSION=5.6.0
+echo "=============== Installing Node $NODE_VERSION ============="
+. /root/.nvm/nvm.sh && nvm install $NODE_VERSION
+nvm use $NODE_VERSION && npm install npm@$NPM_VERSION -g
