acd136c299e92fc6d822721b12ea305b62ca98ef
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-05-17 13:36:16 +0530

    Update _node.sh

--- test/_node.sh
+++ test/_node.sh
@@ -5,20 +5,8 @@ echo "nvm ls"
 nvm ls
 printf "\n"
 
-echo "nvm use 4.8.7"
-nvm use 4.8.7
-printf "\n"
-
-echo "nvm use 5.12.0"
-nvm use 5.12.0
-printf "\n"
-
 echo "nvm use 6.11.5"
 nvm use 6.11.5
-printf "\n"
-
-echo "nvm use 7.10.1"
-nvm use 7.10.1
 printf "\n"
 
 echo "nvm use 8.9.4"
@@ -27,6 +15,10 @@ printf "\n"
 
 echo "nvm use 9.9.0"
 nvm use 9.9.0
+printf "\n"
+
+echo "nvm use 10.1.0"
+nvm use 10.1.0
 printf "\n"
 
 
