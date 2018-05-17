925b83a3c453cc9d8647d1e1c3ad20a5c42cb60f
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-05-17 13:33:18 +0530

    Delete 4_8_4.sh

--- version/4_8_4.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-NODE_VERSION=4.8.7
-NPM_VERSION=5.6.0
-echo "=============== Installing Node $NODE_VERSION ============="
-. /root/.nvm/nvm.sh && nvm install $NODE_VERSION
-nvm use $NODE_VERSION && npm install npm@$NPM_VERSION -g
-
