fb0d80496cc9cb7e2eb4b336db7e8a92ebe5c0af
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-05-21 12:31:52 +0530

    Update and rename 6_11_5.sh to 6_14_2.sh

--- version/6_14_2.sh
+++ version/6_14_2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-NODE_VERSION=6.11.5
+NODE_VERSION=6.14.2
 NPM_VERSION=5.6.0
 echo "=============== Installing Node $NODE_VERSION ============="
 . /root/.nvm/nvm.sh && nvm install $NODE_VERSION
