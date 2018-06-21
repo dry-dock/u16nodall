32eba9f8279195b98cb4981c91597eb20f7d533a
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-06-21 11:46:54 +0530

    Update and rename 9_11_1.sh to 9_11_2.sh

--- version/9_11_2.sh
+++ version/9_11_2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-NODE_VERSION=9.11.1
+NODE_VERSION=9.11.2
 NPM_VERSION=5.6.0
 echo "=============== Installing Node $NODE_VERSION ============="
 . /root/.nvm/nvm.sh && nvm install $NODE_VERSION
