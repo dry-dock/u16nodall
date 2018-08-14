3ac083f8b843ba2f1f8382701a8b0a5afddf87c6
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-08-14 16:13:08 +0530

    Update and rename 10_6_0.sh to 10_8_0.sh

--- version/10_6_0.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-echo "=============== Installing Node v. 10.6.0 ============="
-. /root/.nvm/nvm.sh && nvm install 10.6.0
---
+++ version/10_8_0.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+echo "=============== Installing Node v. 10.8.0 ============="
+. /root/.nvm/nvm.sh && nvm install 10.8.0
