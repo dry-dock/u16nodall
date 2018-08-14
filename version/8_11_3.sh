8c79ad92b5d8f8f5c2b6148424f05666ab6f8c6e
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-08-14 16:43:10 +0530

    Update 8_11_3.sh

--- version/8_11_3.sh
+++ version/8_11_3.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
 
 NODE_VERSION=8.11.3
+NPM_VERSION=6.3.0
 echo "=============== Installing Node $NODE_VERSION ============="
 . /root/.nvm/nvm.sh && nvm install $NODE_VERSION
-nvm use $NODE_VERSION && npm install npm@6.3.0 -g
+nvm use $NODE_VERSION && npm install npm@$NPM_VERSION -g
 nvm alias default 8.11.3
 
 
