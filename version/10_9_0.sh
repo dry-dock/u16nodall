9fc65784f4060e0c153f84dc17918908d6d7e7a8
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-09-19 11:29:44 +0530

    Update 10_9_0.sh

--- version/10_9_0.sh
+++ version/10_9_0.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-NODE_VERSION=10.9.0
-NPM_VERSION=6.4.0
-echo "=============== Installing Node v. 10.9.0 ============="
+NODE_VERSION=10.10.0
+NPM_VERSION=6.4.1
+echo "=============== Installing Node $NODE_VERSION ============="
 . /root/.nvm/nvm.sh && nvm install $NODE_VERSION
 npm install npm@$NPM_VERSION -g
 
