75ab98573e24b058a2aa2e1359f5a60a64595162
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-09-19 11:32:01 +0530

    Update _node.sh

--- test/_node.sh
+++ test/_node.sh
@@ -9,12 +9,12 @@ echo "nvm use 6.14.4"
 nvm use 6.14.4
 printf "\n"
 
-echo "nvm use 8.11.4"
-nvm use 8.11.4
+echo "nvm use 8.12.0"
+nvm use 8.12.0
 printf "\n"
 
-echo "nvm use 10.9.0"
-nvm use 10.9.0
+echo "nvm use 10.10.0"
+nvm use 10.10.0
 printf "\n"
 
 
