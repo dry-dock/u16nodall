6ad689b334813d192246a77313134bdc3e4381dd
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2019-01-04 17:15:27 +0530

    Update and rename 11_1_0.sh to 11_6_0.sh

--- version/11_6_0.sh
+++ version/11_6_0.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-NODE_VERSION=11.1.0
-NPM_VERSION=6.4.1
+NODE_VERSION=11.6.0
+NPM_VERSION=6.5.0
 echo "=============== Installing Node $NODE_VERSION ============="
 . /root/.nvm/nvm.sh && nvm install $NODE_VERSION
 npm install npm@$NPM_VERSION -g
