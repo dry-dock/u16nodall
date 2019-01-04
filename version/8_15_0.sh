9c1f0f8babe63b7885ec968098104fbed964962e
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2019-01-04 17:17:19 +0530

    Update and rename 8_12_0.sh to 8_15_0.sh

--- version/8_15_0.sh
+++ version/8_15_0.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-NODE_VERSION=8.12.0
-NPM_VERSION=6.4.1
+NODE_VERSION=8.15.0
+NPM_VERSION=6.5.0
 echo "=============== Installing Node $NODE_VERSION ============="
 . /root/.nvm/nvm.sh && nvm install $NODE_VERSION
 nvm use $NODE_VERSION && npm install npm@$NPM_VERSION -g
